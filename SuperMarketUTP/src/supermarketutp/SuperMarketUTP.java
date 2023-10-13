82990f4aa2739a0da44531fc2a8ab28c6e0414cd
Bailon18 <paucarmontesbailon@gmail.com>
2023-10-13 12:13:58 -0500

    se subio primer avance

---
+++ SuperMarketUTP/src/supermarketutp/SuperMarketUTP.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package supermarketutp;
+
+import supermarketutp.form.PrincipalForm;
+
+/**
+ *
+ * @author user
+ */
+public class SuperMarketUTP {
+
+
+    public static void main(String[] args) {
+        PrincipalForm principalForm = new PrincipalForm();
+        principalForm.setVisible(true);
+    }
+    
+}
